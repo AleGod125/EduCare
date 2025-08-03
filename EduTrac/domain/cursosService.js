19e8faf86b5bc71a4d7e681bef19c284f898753b
AleGod125 <147008319+AleGod125@users.noreply.github.com>
2025-08-02 21:41:15 -0500

    Add files via upload

---
+++ EduTrac/domain/cursosService.js
@@ -0,0 +1,10 @@
+import { fakeDB } from '/data/fakeDB.js';
+
+export function getCursosEstudiantes(curso) {
+ // console.log(fakeDB.cursos[`${curso}`].estudiantes);
+}
+
+
+export function getCursoName() {
+  return fakeDB.cursos;
+}
---
+++ EduTrac/ui/dashboard/dashboard.js
@@ -0,0 +1,6 @@
+import { getCursosEstudiantes , getCursoName } from '/domain/cursosService.js';
+
+
+console.log(getCursosEstudiantes("Sistemas Operativos"));
+console.log(getCursoName());
+
